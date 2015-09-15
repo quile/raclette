288163098c338912ff5d07ac47c8f0d9594c153e
Kyle Dawkins <quile@users.noreply.github.com>
2015-09-14 17:56:30 -0700

    New extractor for #MartinJones

---
+++ lib/Raclette/Extractor/MartinJones.pm
@@ -0,0 +1,58 @@
+package Raclette::Extractor::MartinJones;
+
+use strict;
+use warnings;
+
+use base qw(Raclette::Extractor);
+
+use Raclette::Utilities;
+
+sub extractTitle {
+    my ($self) = @_;
+    my $title = $self->{_json}->{title};
+    $title =~ m/^([^:]+):/m;
+    return $1 || $self->SUPER::extractTitle();
+}
+
+sub extractComposer {
+    my ($self) = @_;
+    my $desc = $self->{_json}->{description};
+    $desc =~ m/^(.*) \x{b7} (.*)? \x{b7} (.*)?$/m;
+    return $self->normaliseComposer($2 || $self->SUPER::extractComposer());
+}
+
+sub extractPerformers {
+    my ($self) = @_;
+    my $description = $self->{_json}->{description};
+
+    my $desc = $self->{_json}->{description};
+    $desc =~ m/^(.*) \x{b7} (.*)? \x{b7} (.*)?$/m;
+    if ($3) {
+        return [$3];
+    }
+    return []
+}
+
+sub extractSplits {
+    my ($self) = @_;
+
+    my $splits = [];
+
+    my $json = $self->{_json};
+    my $description = $json->{description};
+
+    if ($description =~ m/^(.*) \x{b7} (.*)? \x{b7} (.*)?$/m) {
+        my $title = $1;
+        push @$splits, {
+            start => 0,
+            title => $title,
+            track => undef,
+            source => $1
+        };
+    }
+
+    return $self->populateSplits($splits, $json->{duration}) if scalar @$splits;
+    return $self->SUPER::extractSplits();
+}
+
+1;
--- lib/Raclette/Extractors.pm
+++ lib/Raclette/Extractors.pm
@@ -5,6 +5,15 @@ use Try::Tiny;
 use Raclette::Extractor;
 use Raclette::Extractor::UnsungMasterworks;
 use Raclette::Extractor::ComposersbyNumbers;
+use Raclette::Extractor::KuhlauDilfeng2;
+use Raclette::Extractor::MartinJones;
+
+sub _sanitisedUploaderName {
+    my ($uploader) = @_;
+
+    $uploader =~ s/[^_A-Z0-9]//ig;
+    return $uploader;
+}
 
 sub extractorForJSON {
     my ($class, $json) = @_;
@@ -12,7 +21,7 @@ sub extractorForJSON {
     my $n = undef;
 
     try {
-        my $ec = "Raclette::Extractor::".$json->{uploader};
+        my $ec = "Raclette::Extractor::"._sanitisedUploaderName($json->{uploader});
         $n = $ec->new($json);
     } catch {
         print STDERR "No extractor found for user ".$json->{uploader}."\n";
